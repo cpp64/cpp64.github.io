019841180f41edaf434175d595fda1e8d0169635
cpp64 <56206840+cpp64@users.noreply.github.com>
2024-04-12 18:09:20 +0400

    Update script.js

--- schedule/script.js
+++ schedule/script.js
@@ -1,10 +1,10 @@
 // количество корпусов, дней, уроков в день, кабинетов, классов в каждой параллели
-let Ncorp, Ndays, Nlessons, Ncab = 9999, Nparallel = [];
+let Ncorp, Nlessons = [], Ncab = 9999, Nparallel = [];
 function fake_click() {
         console.log("fake_click");
 	Ncorp = Number(document.getElementById("Ncorp_input").value);
-	Ndays = Number(document.getElementById("Ndays_input").value);
-	Nlessons = Number(document.getElementById("Nlessons_input").value);
+	for(let i = 1; i < 7; ++i)
+		Nlessons = Number(document.getElementById("Nlessons_input"+i.toString()).value);
 	var fr = new FileReader();
 	fr.onload = function () {
 		Nparallel = parseCSV(new Uint8Array(fr.result));
