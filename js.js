32141e4f6c2ac4dd53640d56daaf5411841763bb
cpp64 <56206840+cpp64@users.noreply.github.com>
2024-10-19 14:42:47 +0400

    Update js.js

--- js.js
+++ js.js
@@ -11,3 +11,8 @@ $(function () {
 });
 /jquery-3.7.1.min.js
 <div id="navigation"></div>
+
+                <meta charset="UTF-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <link rel="stylesheet" type="text/css" href="/style.css" />
+                <link rel="icon" type="image/png" sizes="192x192" href="/icons/dove32w2.ico" />
