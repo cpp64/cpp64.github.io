6bb49cc6378fe5e763b21ca6e6f668c2ddbc2ff2
cpp64 <56206840+cpp64@users.noreply.github.com>
2024-10-19 15:30:58 +0400

    Update js.js

--- js.js
+++ js.js
@@ -7,20 +7,20 @@ document.head.insertBefore(meta_charset, document.head.firstChild);
 var meta_viewport = document.createElement('meta');
 meta_viewport.name = "viewport";
 meta_viewport.content = "width = device-width, initial-scale = 1.0";
-document.head.insertBefore(meta_viewport, document.body.firstChild);
+document.head.insertBefore(meta_viewport, document.head.firstChild);
 
 var link_style = document.createElement('link');
 link_style.rel = "stylesheet";
 link_style.type = "text/css";
 link_style.href = "/style.css";
-document.head.insertBefore(link_style, document.body.firstChild);
+document.head.insertBefore(link_style, document.head.firstChild);
 
 var link_icon = document.createElement('link');
 link_icon.rel = "icon";
 link_icon.type = "image/png";
 link_icon.sizes = "192x192";
 link_icon.href = "/icons/dove32w2.ico";
-document.head.insertBefore(link_icon, document.body.firstChild);
+document.head.insertBefore(link_icon, document.head.firstChild);
 
 /// BODY
 
@@ -31,7 +31,7 @@ document.body.append(script);
 var div_navigation = document.createElement('div');
 div_navigation.id = "navigation";
 div_navigation.class = "navigation";
-document.head.insertBefore(div_navigation, document.body.firstChild);
+document.body.insertBefore(div_navigation, document.body.firstChild);
 $(function () {
         $("#navigation").load("/navigation.html");
 });
