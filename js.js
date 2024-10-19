8b638e461ad0a5265efddd98a0c31181cec66068
cpp64 <56206840+cpp64@users.noreply.github.com>
2024-10-19 14:48:17 +0400

    Update js.js

--- js.js
+++ js.js
@@ -12,7 +12,14 @@ $(function () {
 /jquery-3.7.1.min.js
 <div id="navigation"></div>
 
-                <meta charset="UTF-8" />
-                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                <link rel="stylesheet" type="text/css" href="/style.css" />
-                <link rel="icon" type="image/png" sizes="192x192" href="/icons/dove32w2.ico" />
+<link rel="stylesheet" type="text/css" href="/style.css" />
+<link rel="icon" type="image/png" sizes="192x192" href="/icons/dove32w2.ico" />
+
+var meta_charset = document.createElement('meta');
+meta_charset.charset = "UTF-8";
+document.head.insertBefore(meta_charset, container.head.body);
+
+var meta_viewport = document.createElement('meta');
+meta_viewport.name = "viewport";
+meta_viewport.content = "width=device-width, initial-scale=1.0";
+document.head.insertBefore(meta_viewport, container.head.body);
