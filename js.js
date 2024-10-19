86378339d0b76a059006ab75506cdaced4c3c0dc
cpp64 <56206840+cpp64@users.noreply.github.com>
2024-10-19 15:22:52 +0400

    Update js.js

--- js.js
+++ js.js
@@ -1,3 +1,36 @@
+var script = document.createElement("script");
+script.src = "/jquery-3.7.1.min.js";
+document.body.append(script);
+
+var meta_charset = document.createElement('meta');
+meta_charset.charset = "UTF-8";
+document.head.insertBefore(meta_charset, container.head.body.firstChild);
+
+var meta_viewport = document.createElement('meta');
+meta_viewport.name = "viewport";
+meta_viewport.content = "width=device-width, initial-scale=1.0";
+document.head.insertBefore(meta_viewport, container.head.body.firstChild);
+
+var link_style = document.createElement('link');
+link_style.rel = "stylesheet";
+link_style.type = "text/css";
+link_style.href = "/style.css";
+document.head.insertBefore(link_style, container.head.body.firstChild);
+
+var link_icon = document.createElement('link');
+link_icon.rel = "icon";
+link_icon.type = "image/png";
+link_icon.sizes = "192x192";
+link_icon.href = "/icons/dove32w2.ico";
+document.head.insertBefore(link_icon, container.head.body.firstChild);
+
+var div_navigation = document.createElement('div');
+div_navigation.id = "navigation";
+div_navigation.class = "navigation";
+$(function () {
+        $("#navigation").load("/navigation.html");
+});
+
 function myFunction() {
         var x = document.getElementById("mynavigation");
         if (x.className === "navigation") {
@@ -6,35 +39,3 @@ function myFunction() {
                 x.className = "navigation";
         }
 }
-$(function () {
-        $("#navigation").load("/navigation.html");
-});
-/jquery-3.7.1.min.js
-
-
-var meta_charset = document.createElement('meta');
-meta_charset.charset = "UTF-8";
-document.head.insertBefore(meta_charset, container.head.body);
-
-var meta_viewport = document.createElement('meta');
-meta_viewport.name = "viewport";
-meta_viewport.content = "width=device-width, initial-scale=1.0";
-document.head.insertBefore(meta_viewport, container.head.body);
-
-var link_style = document.createElement('link');
-link_style.rel = "stylesheet";
-link_style.type = "text/css";
-link_style.href = "/style.css";
-document.head.insertBefore(link_style, container.head.body);
-
-var link_icon = document.createElement('link');
-link_icon.rel = "icon";
-link_icon.type = "image/png";
-link_icon.sizes = "192x192";
-link_icon.href = "/icons/dove32w2.ico";
-document.head.insertBefore(link_icon, container.head.body);
-
-var div_navigation = document.createElement('div');
-div_navigation.id="navigation"
-div_navigation,
-div_navigation,
