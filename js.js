e8070ae8700a67575e5ba743e8ca4e37c53b78be
cpp64 <56206840+cpp64@users.noreply.github.com>
2024-10-19 15:25:08 +0400

    Update js.js

--- js.js
+++ js.js
@@ -4,29 +4,30 @@ document.body.append(script);
 
 var meta_charset = document.createElement('meta');
 meta_charset.charset = "UTF-8";
-document.head.insertBefore(meta_charset, container.head.body.firstChild);
+document.head.insertBefore(meta_charset, container.body.firstChild);
 
 var meta_viewport = document.createElement('meta');
 meta_viewport.name = "viewport";
 meta_viewport.content = "width=device-width, initial-scale=1.0";
-document.head.insertBefore(meta_viewport, container.head.body.firstChild);
+document.head.insertBefore(meta_viewport, container.body.firstChild);
 
 var link_style = document.createElement('link');
 link_style.rel = "stylesheet";
 link_style.type = "text/css";
 link_style.href = "/style.css";
-document.head.insertBefore(link_style, container.head.body.firstChild);
+document.head.insertBefore(link_style, container.body.firstChild);
 
 var link_icon = document.createElement('link');
 link_icon.rel = "icon";
 link_icon.type = "image/png";
 link_icon.sizes = "192x192";
 link_icon.href = "/icons/dove32w2.ico";
-document.head.insertBefore(link_icon, container.head.body.firstChild);
+document.head.insertBefore(link_icon, container.body.firstChild);
 
 var div_navigation = document.createElement('div');
 div_navigation.id = "navigation";
 div_navigation.class = "navigation";
+document.head.insertBefore(div_navigation, container.body.firstChild);
 $(function () {
         $("#navigation").load("/navigation.html");
 });
